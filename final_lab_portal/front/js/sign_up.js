515b5955d947d74d4d10bb0189e67c7211ddd9dd
Panos Livadaros <panoslivadaros@gmail.com>
2025-01-29 19:07:27 +0200

    Add files via upload

---
+++ final_lab_portal/front/js/sign_up.js
@@ -0,0 +1,39 @@
+const signupForm = document.getElementById("signupForm");
+const responseMessage = document.getElementById("responseMessage");
+
+const API_URL = "http://localhost:3000"; // Replace with your backend URL
+
+// Helper function to display messages
+function showMessage(message, isError = false) {
+  responseMessage.textContent = message;
+  responseMessage.className = isError ? "message error" : "message";
+}
+
+// Handle Sign-Up
+signupForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const firstName = document.getElementById("signupFirstName").value;
+  const lastName = document.getElementById("signupLastName").value;
+  const email = document.getElementById("signupEmail").value;
+  const password = document.getElementById("signupPassword").value;
+
+  try {
+    const response = await fetch(`${API_URL}/signup`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ firstName, lastName, email, password }),
+    });
+    const data = await response.json();
+
+    if (response.ok) {
+      //here you get the data.uid
+      showMessage("Sign-up successful!");
+      window.location.href = '/front/html/log_in.html';
+    } else {
+      showMessage(data.error, true);
+    }
+  } catch (error) {
+    showMessage("Error: " + error.message, true);
+  }
+});
