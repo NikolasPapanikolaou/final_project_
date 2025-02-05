89ad8e71da4476639fa64a869c058d06e0cafdf4
nikolas_papanikolaou <92992576+NikolasPapanikolaou@users.noreply.github.com>
2025-02-05 19:35:38 +0200

    Update app.js

--- final_lab_portal/back/app.js
+++ final_lab_portal/back/app.js
@@ -6,7 +6,11 @@ const socketIo = require("socket.io");
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+  origin: ["http://127.0.0.1:5500", "http://localhost:5500"], // Allow your frontend
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true
+}));
 app.use(express.json());
 
 const { admin, firebase, auth, db } = require("./firebase");
